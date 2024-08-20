decb7cb8fead41ba4b5f64ae5f90d69437948645
kkent030315 <hrn832@protonmail.com>
2024-08-21 01:19:34 +0900

    part 2

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # ANY.RUN Sandbox Detection #1
 
+- Part 1: The symblic device name of the driver.
+- Part 2: The non-standard minifilter behaviour under protected ANY.RUN installed files and folders
+
 ![IMG](.png)
 
 <sup>
-Part 2: Coming Soon
+Part <u>3</u>: Coming Soon
 </sup>
 
--- detect.cc
+++ detect.cc
@@ -1,13 +1,18 @@
 
-#include <windows.h>
+#define PHNT_VERSION PHNT_WIN11
+#include <phnt_windows.h>
+#include <phnt.h>
+
 #include <iostream>
+
+#pragma comment(lib, "ntdll.lib")
 
 #define ANYRUN_DRIVER_DEVICE_NAME "\\\\?\\\\A3E64E55_fl"
 
 static bool detect_anyrun()
 {
     HANDLE hFile;
-
+    
     hFile = CreateFile(
         /*lpFileName*/TEXT(ANYRUN_DRIVER_DEVICE_NAME),
         /*dwDesiredAccess*/GENERIC_READ,
@@ -28,9 +33,50 @@ static bool detect_anyrun()
     return true;
 }
 
+static int detect_anyrun2()
+{
+    NTSTATUS status;
+
+    UNICODE_STRING name;
+    RtlInitUnicodeString(&name, L"\\??\\C:\\Program Files\\KernelLogger");
+
+    HANDLE hFile;
+    IO_STATUS_BLOCK iosb = { 0 };
+    OBJECT_ATTRIBUTES attrs;
+    InitializeObjectAttributes(&attrs, &name, 0, NULL, NULL);
+
+    status = NtCreateFile(
+        /*FileHandle*/&hFile,
+        /*DesiredAccess*/GENERIC_READ | SYNCHRONIZE,
+        /*ObjectAttributes*/&attrs,
+        /*IoStatusBlock*/&iosb,
+        /*AllocationSize*/NULL,
+        /*FileAttributes*/FILE_ATTRIBUTE_DIRECTORY,
+        /*ShareAccess*/FILE_SHARE_READ,
+        /*CreateDisposition*/FILE_OPEN,
+        /*CreateOptions*/FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT,
+        /*EaBuffer*/NULL,
+        /*EaLength*/0
+    );
+
+    // ANY.RUN minifilter returns non-standard status code, STATUS_NO_SUCH_FILE
+    // If this status code is returned, it means that the directory is protected
+    // by the ANY.RUN minifilter driver.
+    // To patch this detection, I would recommend returning STATUS_OBJECT_NAME_NOT_FOUND
+    // that is a standard status code for this situation.
+    if (status == STATUS_NO_SUCH_FILE)
+        return true;
+
+    // Not actually the case, maybe conflict with other software installation.
+    if (NT_SUCCESS(status))
+        NtClose(hFile);
+
+    return false;
+}
+
 int main()
 {
-    if (detect_anyrun())
+    if (detect_anyrun() || detect_anyrun2())
         wprintf(L"AnyRun detected\n");
     else
         wprintf(L"You're good to go\n");
